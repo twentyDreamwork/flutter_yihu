8d8c7bd193b00c516fcf9b312277802e6bac6128
wuzhipeng <471895027@qq.com>
2019-11-11 09:24:50 +0800

    update

--- lib/config/index.dart
+++
@@ -1,5 +0,0 @@
-export 'color.dart';
-export 'font.dart';
-export 'string.dart';
-export 'length.dart';
-export 'http_conf.dart';
--- lib/model/UserInfo.dart
+++
@@ -1,9 +0,0 @@
-// 用户信息
-class UserInfo {
-  String nickname;
-  String unionid;
-  String headimgurl;
-  String phone;
-
-  UserInfo({this.nickname, this.unionid, this.headimgurl, this.phone});
-}
